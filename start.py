4bbad1e354c690de072ccb8e0fe755041bb4d2d7
Umidjon Xusanov <u.xusanov@newuu.uz>
2026-06-03 10:20:44 +0500

    commit

--- start.py
+++ start.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
 Start-up helper:
-Launches the scheduler as a background process.
+Launches the scraper as a background process.
 """
 import subprocess
 import sys
 
 if __name__ == "__main__":
-    subprocess.run([sys.executable, "-u", "-m", "app.scheduler"])
+    subprocess.run([sys.executable, "-u", "-m", "app.scraper"])
+
 
